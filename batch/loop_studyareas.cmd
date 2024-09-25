56b364fe389d4829b2a461003b47cb0648c00439
Jip Claassens <jclaassens@objectvision.nl>
2024-09-25 15:22:12 +0200

    update batch file

--- batch/loop_studyareas.cmd
+++ batch/loop_studyareas.cmd
@@ -1,14 +1,24 @@
-set pf="Program Files"
-set config=D:\ProjDir\2BURP\cfg
-set geodmsversion=GeoDMS15.4.0
+
+REM Setup Chris
+REM set config=D:\ProjDir\2BURP\cfg
+
+REM Setup OVSRV06
+set config=C:\ProjDir\Jip\2BURP\cfg
+
+set geodmsversion=GeoDMS15.7.1
+set exe_dir=C:\Program Files\ObjectVision\%geodmsversion%
+set ProgramPath=%exe_dir%\GeoDmsRun.exe
+
+cd ..\cfg
 
 set studyarealist=Europe South_America Africa Asia North_America Australia_Oceania
+REM set studyarealist=Netherlands 
 
 for %%s in (%studyarealist%) do (
-	echo %%s> %config%\studyarea.txt
+	echo %%s 
 	
-	C:\%pf%\ObjectVision\%geodmsversion%\GeoDmsRun.exe %config%\main.dms /Preprocessing/DegreesOfUrbanisation/Generate_DegreesOfUrbanisation 
-	C:\%pf%\ObjectVision\%geodmsversion%\GeoDmsRun.exe %config%\main.dms /Analysis/Future/Indicators/Prep/Generate_All	
-	
+	set StudyArea=%%s
+	"%ProgramPath%" %config%\main.dms /Preprocessing/DegreesOfUrbanisation/Generate_DegreesOfUrbanisation 
+	"%ProgramPath%" %config%\main.dms /Analysis/Future/Indicators/Prep/Generate_All
 )
 pause
